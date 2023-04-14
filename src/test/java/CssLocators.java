c0fadb98e6f4b38f4bf4768402b9ecda90a4216d
khaledoni01 <khaledoni01@gmail.com>
2023-04-14 19:49:24 -0400

    css-selector

---
+++ src/test/java/CssLocators.java
@@ -0,0 +1,25 @@
+public class CssLocators {
+
+    String emailField = "input[type='email']";
+
+    String passwordField = "input[type='password']";
+
+    String loginButton = "button[type='submit']";
+
+    String searchField = "input[type='search']";
+
+    String logoutButton = ".logout.control";
+
+    String favoritesPlaylist = "#playlists .favorites";
+
+    String lyricsTab = "#extraTabLyrics";
+
+    String howdy = "#homeWrapper header h1";
+
+    String mostPlayedHeader = ".two-cols section:nth-child(1) h1";
+
+    String volumeRange = "#volume.volume.control";
+
+    String playButton = "[title='Play or resume']";
+
+}
