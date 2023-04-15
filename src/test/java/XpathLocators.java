b502782f616bc596e63a3b1e84666e2966a2c8fc
khaledoni01 <khaledoni01@gmail.com>
2023-04-14 22:33:54 -0400

    xpath-locators

---
+++ src/test/java/XpathLocators.java
@@ -0,0 +1,21 @@
+public class XpathLocators {
+
+
+    String search = "//input[@type='search']";
+
+    String logout = "//a[@class='logout control']";
+
+    String favorites = "//section[@id='playlists']//li[@class='playlist favorites']";
+
+    String lyrics = "//button[@id='extraTabLyrics']";
+
+    String howdy = "//section[@id='homeWrapper']/header//h1";
+
+    String mostPlayed = "//h1[contains(text(), 'Most Played')]";
+
+    String volumeRange = "//span[@id='volume']";
+
+    String playButton = "//span[@title='Play or resume']";
+
+
+}
