b10541c915bf15b7c09d79767ec559f4edf89eea
khaledoni01 <khaledoni01@gmail.com>
2023-04-20 10:13:50 -0400

    test merge

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -9,8 +9,7 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
-
-
+    
     @Test
     public void loginInvalidEmailValidPasswordTest(){
         // Pre-condition
