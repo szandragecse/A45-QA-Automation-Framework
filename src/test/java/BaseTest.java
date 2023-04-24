e4767dfbe6b3b5815a7aed91f8f39b98d5bcd9f6
khaledoni01 <khaledoni01@gmail.com>
2023-04-24 17:28:45 -0400

    refactored-code

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,55 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class BaseTest {
+
+    public static WebDriver driver = null;
+    public static String url = "https://bbb.testpro.io/";
+    @BeforeMethod
+    public void launchBrowser() {
+        // Pre-condition
+        //      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(url);
+    }
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    public static void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.click();//not needed
+        emailField.clear();
+        emailField.sendKeys(email);
+
+    }
+    public static void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.click();//not needed
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+    public static void clickSubmit() {
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
     }
 }
