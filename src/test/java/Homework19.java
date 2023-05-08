7c6c23ed7f724652e4b990396f8b904f10e251ea
szandragecse <szandra.gecse@testpro.io>
2023-05-07 20:22:28 -0400

    Homework#19 delete a playlist

--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -1,14 +1,13 @@
 import org.testng.annotations.Test;
 
-public class Homework18 extends BaseTest{
+public class Homework19 extends BaseTest{
     @Test
     public static void deletePlaylist(){
         provideEmail("sandra.geche@gmail.com");
         providePassword("te$t$tudent");
         clickSubmit();
-chooseDesiredPlaylist();
-deletePlaylistButton();
-deletePlaylistOk();
+        chooseDesiredPlaylist();
+        deletePlaylistButton();
+        deletePlaylistOk();
     }
 }
-
